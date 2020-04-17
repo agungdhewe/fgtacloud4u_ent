e942bff5bd2280845bf0228a19eb1cec7b735920
agungdhewe <agung.dhewe@yahoo.com>
2020-04-17 08:32:41 +0700

    update master deptgroup

--- organisation/deptgroup/deptgroup.sql
+++ organisation/deptgroup/deptgroup.sql
@@ -27,4 +27,108 @@ ALTER TABLE `mst_deptgroup` ADD CONSTRAINT `fk_mst_deptgroup_mst_depttype` FOREI
 
 
 
+DROP TRIGGER IF EXISTS kalistadb.mst_deptgroup_before_insert;
+
+DELIMITER $$
+$$
+CREATE DEFINER=`root`@`localhost` TRIGGER mst_deptgroup_before_insert
+BEFORE INSERT
+   ON mst_deptgroup FOR EACH ROW
+   
+BEGIN
+
+	DECLARE PATHID VARCHAR(13);
+	DECLARE PARENT_PATHID VARCHAR(13);
+	DECLARE PARENT_PATH VARCHAR(390);
+
+	IF NEW.deptgroup_parent=NEW.deptgroup_id THEN
+		SIGNAL SQLSTATE '45000' SET 
+		MESSAGE_TEXT = 'Kode parent tidak boleh sama dengan kode group';
+	END IF;
+
+	SET NEW.deptgroup_pathid = NEW.deptgroup_id;
+
+	SET PATHID = RPAD(NEW.deptgroup_pathid, 13, '-');
+	SET NEW.deptgroup_pathid = PATHID;
+
+	IF NEW.deptgroup_parent IS NULL THEN
+		SET NEW.deptgroup_path = PATHID;
+		SET NEW.deptgroup_level = 0;
+	ELSE
+		SELECT deptgroup_pathid, deptgroup_path 
+		INTO PARENT_PATHID, PARENT_PATH
+		FROM mst_deptgroup WHERE deptgroup_id = NEW.deptgroup_parent;	
+			
+		SET NEW.deptgroup_path = CONCAT(PARENT_PATH, PARENT_PATHID);
+		SET NEW.deptgroup_level = (LENGTH(NEW.deptgroup_path) / 13) - 1;
+	
+	END IF;
+
+END
+
+$$
+DELIMITER ;
+
+
+
+
+
+
+DROP TRIGGER IF EXISTS kalistadb.mst_group_before_update;
+
+DELIMITER $$
+$$
+CREATE DEFINER=`root`@`localhost` TRIGGER `mst_group_before_update` BEFORE UPDATE ON `mst_deptgroup` FOR EACH ROW BEGIN
+
+
+	DECLARE CHILDCOUNT INT;
+	DECLARE PARENT_PATHID VARCHAR(13);
+	DECLARE PARENT_PATH VARCHAR(390);
+
+
+	SET NEW.deptgroup_id = OLD.deptgroup_id;
+	SET NEW.deptgroup_pathid = OLD.deptgroup_pathid;
+
+	IF NEW.deptgroup_parent=NEW.deptgroup_id THEN
+		SIGNAL SQLSTATE '45000' SET 
+		MESSAGE_TEXT = 'Kode parent tidak boleh sama dengan kode group';
+	END IF;
+	
+
+	IF NEW.deptgroup_parent='--NULL--' THEN
+    	SET NEW.deptgroup_parent = NULL;
+    END IF;
+
+
+	SELECT COUNT(*) 
+	INTO CHILDCOUNT
+	FROM mst_deptgroup WHERE deptgroup_parent = NEW.deptgroup_id;
+	IF NEW.deptgroup_parent<>OLD.deptgroup_parent AND CHILDCOUNT>0 THEN
+		SIGNAL SQLSTATE '45000' SET 
+		MESSAGE_TEXT = 'Group yang punya anggota tidak bisa diubah parentnya';	
+	END IF;
+
+
+	IF NEW.deptgroup_parent IS NULL THEN
+		SET NEW.deptgroup_path = NEW.deptgroup_pathid;
+		SET NEW.deptgroup_level = 0;
+	ELSE
+		SELECT deptgroup_pathid, deptgroup_path 
+		INTO PARENT_PATHID, PARENT_PATH
+		FROM mst_deptgroup WHERE deptgroup_id = NEW.deptgroup_parent;	
+			
+		SET NEW.deptgroup_path = CONCAT(PARENT_PATH, PARENT_PATHID);
+		SET NEW.deptgroup_level = (LENGTH(NEW.deptgroup_path) / 13) - 1;
+	
+	END IF;
+
+END
+
+$$
+DELIMITER ;
+
+
+
+
+
 
