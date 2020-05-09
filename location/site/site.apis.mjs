0adc23ab8513e9eaf143615bfb09bbd5850b934a
agungdhewe <agung_dhewe@yahoo.com>
2020-05-09 10:11:07 +0700

    add master site

---
+++ location/site/site.apis.mjs
@@ -0,0 +1,20 @@
+/* API yang akan digunakan di program
+
+
+	export const testapi = 'fgta/framework/formdev/nama-api'
+
+
+kemdian, di program bisa diakses dengan
+
+	$ui.apis.testapi
+
+*/
+
+export const load_sitemodel_id = 'ent/location/sitemodel/list'
+export const load_sitegroup_id = 'ent/location/sitegroup/list'
+export const load_land_id = 'ent/location/land/list'
+export const load_dept_id = 'ent/organisation/dept/list'
+export const load_unit_id = 'ent/organisation/unit/list'
+
+
+
